df75af6138efb61be2f01bdbf31813d708e4538d
wildomca <omsingh10@gmail.com>
2022-11-03 21:42:13 +0530

    new change in feature

--- src/Program.java
+++ src/Program.java
@@ -3,7 +3,9 @@ public class Program {
 
 	public static void main(String[] args) {
 	
-		System.out.println("We will won the world cup");
+		System.out.println("We will won the world cup ");
+		
+		System.out.println("This is the new Change");
 
 	}
 
